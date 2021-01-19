24e4ad75f9e6234f622177e5b57fe32417784037
taewoo_lee <gunmong83@gmail.com>
2021-01-19 17:19:00 +0900

    arm64 init

---
+++ sdk/native/jni/OpenCV-arm64-v8a.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=cpufeatures libprotobuf libjpeg-turbo libwebp libpng libtiff libjasper IlmImf quirc tegra_hal
+OPENCV_EXTRA_COMPONENTS:=C:/Users/gunmo/AppData/Local/Android/Sdk/ndk/20.1.5948944/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/aarch64-linux-android/libz.a dl m log
